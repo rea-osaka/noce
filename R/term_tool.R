eb26a31296fbf27cca939b626c8b1734d2fffd6c
Syunsuke Fukuda <syunsuke.fukuda@gmail.com>
2020-07-11 23:55:37 +0900

    improve logic for date term
    期間区分を文字列から日付オブジェクトに変更

--- R/term_tool.R
+++ R/term_tool.R
@@ -10,47 +10,32 @@
 noce_add_terms <- function(df){
   ans <- df %>%
     dplyr::mutate(year_term = .to_year_term(.data$date),
-           quater_term = .to_quarter_year_term(.data$date),
-           month_term = .to_month_term(.data$date))
+                  quarter_term = .to_quarter_year_term(.data$date),
+                  month_term = .data$date)
   
   return(ans)
 }
 
 .to_quarter_year_term <- function(date_objs){
-  n <- length(date_objs)
-  ans <- vector("character",n)
+
+  tmpq <- lubridate::quarter(date_objs)
   
-  for(i in seq_along(date_objs)){
-    ans[i] <- .to_quarter_year_single(date_objs[i])
-  }
+  tmpm <- ifelse(tmpq == 1, 1,
+                ifelse(tmpq == 2, 4,
+                       ifelse(tmpq == 3, 7,10 )))
+  ans <- date_objs
+  lubridate::month(ans) <- tmpm
+  lubridate::day(ans) <- 1
   
   return(ans)
-}
-
-.to_month_term <- function(date_objs){
-  sprintf("%d%02d", lubridate::year(date_objs), lubridate::month(date_objs))
+  
 }
 
 
 .to_year_term <- function(date_objs){
-  lubridate::year(date_objs) %>% as.character() 
-}
-
-.to_quarter_year_single <- function(date_obj){
-  year <- lubridate::year(date_obj)
-  month <- lubridate::month(date_obj)
-  
-  ans <- NULL
-  
-  if(month >= 1 && month <= 3){
-    ans <- sprintf("%dQ1", year)
-  }else if(month >= 4 && month <= 6){
-    ans <- sprintf("%dQ2", year)
-  }else if(month >= 7 && month <= 9){
-    ans <- sprintf("%dQ3", year)
-  }else{
-    ans <- sprintf("%dQ4", year)
-  }
-  
+  ans <- date_objs
+  lubridate::month(ans) <- 1
+  lubridate::day(ans) <- 1
   return(ans)
 }
+
